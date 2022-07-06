444441d956445a63b848a8355de8e847aac74ef7
Gogo Vasev <gvasevg@gmail.com>
2022-07-06 18:04:18 +0300

    initial project

---
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,10 @@
+package com.example.demo.student;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+
+public class Student {
+
+}
---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+
+public class StudentConfig {
+
+}
