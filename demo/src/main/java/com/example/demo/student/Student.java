4193e8a2deb4bde187bd6c6c6dd0200fd7394678
Gogo Vasev <gvasevg@gmail.com>
2022-07-07 17:24:36 +0300

    student class update

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -4,7 +4,85 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
 
-
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    @Transient
+    private Integer age;
 
+    public Student() {
+    }
+
+    public Student(Long id,
+                   String name,
+                   LocalDate dob,
+                   Integer age) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name,
+                   LocalDate dob,
+                   Integer age) {
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
 }
